abcfd48e28c2704db01aec564e9e2f38091b85cb
W_Kazdel <lovelycc.pig@gmail.com>
2020-09-02 22:02:08 +0800

    Fix occupancy on_load

--- Monitor.py
+++ Monitor.py
@@ -7,7 +7,7 @@ import shutil
 sleep = 12
 
 fp_x = 0
-fp_y = 0
+fp_y = 70
 fp_z = 0
 
 json_filename = "./records/record_list.json"
@@ -16,41 +16,69 @@ record_list = []
 
 ISOTIMEFORMAT = '%Y-%m-%d %H.%M.%S'
 
+count = 0
+
+status = 0
+
 def on_load(server, old):
     global record_list
-    monitor(server)
+    apart()
 
 def saveJson():
-    with open(json_filename, 'w') as f:
+    with open(json_filename, 'w+') as f:
         json.dump(record_list, f, indent=4)
 
-def monitor(server):
+def apart():
     theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
-    shutil.copy(json_filename, './records/' + str(theTime) + '.json')
+    try:
+        shutil.copy(json_filename, './records/' + str(theTime) + '.json')
+    except:
+        saveJson()
+
+def on_player_joined(server, player):
+    global count
+    global status
+    count = count + 1
+    if count == 1:
+        status = 1
+        monitor(server)
+
+def on_player_left(server, player):
+    global count
+    global status
+    count = count - 1
+    if count == 0:
+        status = 0
+
+def monitor(server):
     PI = server.get_plugin_instance('PlayerInfoAPI')
     OP = server.get_plugin_instance('OnlinePlayerAPI')
     while True:
-        players = OP.get_player_list()
-        try:
-            for i in range(len(players)):
-                pos = PI.getPlayerInfo(server, players[i], path='Pos')
-                x = int(pos[0])
-                y = int(pos[1])
-                z = int(pos[2])
-                dim = PI.getPlayerInfo(server, players[i], path='Dimension')
-                pre = " 危 "
-                if fp_x-50<=x<=fp_x+50 and fp_z-50<z<fp_z+50 and fp_y-10<=y<=fp_y+10 and dim==-1:
-                    server.say("§c"+ "Warning!" + pre + players[i] + pre + "在伪和平传送门附近游荡！！！")
-                if dim == -1:
-                    dim = "Nether"
-                elif dim == 0:
-                    dim = "World"
-                elif dim == 1:
-                    dim = "End"
-                theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
-                info = str(theTime) + " " + str(players[i]) + " " + str(dim) + " x:" + str(x)+ " y:" + str(y)+ " z:" + str(z)
-                record_list.append(info)
-                saveJson()
-        except:
-            continue
-        time.sleep(sleep)
+        if status == 1:
+            time.sleep(3)
+            players = OP.get_player_list()
+            try:
+                for i in range(len(players)):
+                    pos = PI.getPlayerInfo(server, players[i], path='Pos')
+                    x = int(pos[0])
+                    y = int(pos[1])
+                    z = int(pos[2])
+                    dim = PI.getPlayerInfo(server, players[i], path='Dimension')
+                    pre = " 危 "
+                    if fp_x-50<=x<=fp_x+50 and fp_z-50<z<fp_z+50 and fp_y-10<=y<=fp_y+10 and dim==-1:
+                        server.say("§c"+ "Warning!" + pre + players[i] + pre + "在伪和平传送门附近游荡！！！")
+                    if dim == -1:
+                        dim = "Nether"
+                    elif dim == 0:
+                        dim = "World"
+                    elif dim == 1:
+                        dim = "End"
+                    theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
+                    info = str(theTime) + " " + str(players[i]) + " " + str(dim) + " x:" + str(x)+ " y:" + str(y)+ " z:" + str(z)
+                    record_list.append(info)
+                    saveJson()
+            except:
+                continue
+            time.sleep(sleep-3)
+        elif status == 0:
+            break
--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 
 在玩家靠近伪和平时向全服通知
 
+插件依赖[PlayerInfoAPI](https://github.com/TISUnion/PlayerInfoAPI)和[OnlinePlayerAPI](https://github.com/zhang-anzhi/MCDReforgedPlugins/tree/master/OnlinePlayerAPI)
+
 使用前先进行设置
 
 sleep = 12 每12秒记录一次玩家坐标
 
 fp_x = 0
 
-fp_y = 0
+fp_y = 70
 
 fp_z = 0
 
-伪和平的地狱坐标为 0 0 0
+伪和平的地狱坐标为 0 70 0
