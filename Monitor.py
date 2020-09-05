3a73f1d6d2c415929902202e8d1fe88fb46bccfe
W_Kazdel <lovelycc.pig@gmail.com>
2020-09-05 13:38:51 +0800

    Monitor 2.0

--- Monitor.py
+++ Monitor.py
@@ -6,28 +6,53 @@ import shutil
 
 sleep = 12
 
-fp_x = 0
-fp_y = 70
-fp_z = 0
-
 json_filename = "./records/record_list.json"
+site_info = "./config/site.json"
 
 record_list = []
+site_list = {}
+
+help_msg = '''----------- §aMCDR 监控插件帮助信息 §f-----------
+§b!!mr help §f- §c显示帮助消息
+§b!!mr add [坐标名] [x] [y] [z] [次元] §f- §c次元：world nether end
+§b!!mr list §f- §c显示所有已有监控
+§b!!mr reload §f- §c重载监控坐标，联系管理员删除后操作该指令
+§e为防止随意删除更改监控点，删除监控请联系管理，玩家仅有添加权限
+-----------------Monitor-----------------'''
 
 ISOTIMEFORMAT = '%Y-%m-%d %H.%M.%S'
+pre = " 危 "
 
 count = 0
-
 status = 0
 
+def on_info(server, info):
+    if info.is_player == 1:
+        if info.content.startswith('!!mr'):
+            args = info.content.split(' ')
+            if len(args) == 1:
+                for line in help_msg.splitlines():
+                    server.tell(info.player, line)
+            elif args[1] == 'help':
+                for line in help_msg.splitlines():
+                    server.tell(info.player, line)
+            elif args[1] == 'add':
+                add_site(server, args, info)
+                saveSite()
+            elif args[1] == 'list':
+                show_site(server)
+            elif args[1] == 'reload':
+                load_site(site_info)
+                server.say("§a监控坐标已重载")
+            else:
+                server.tell(info.player, "§7[§aMonitor§f/§cWARN§7] §c参数错误，请输入!!mr 查看帮助信息")
+
 def on_load(server, old):
-    global record_list
+    server.add_help_message('!!mr', '监控插件')
     apart()
+    load_site(site_info)
 
-def saveJson():
-    with open(json_filename, 'w+') as f:
-        json.dump(record_list, f, indent=4)
-
+# 分割文件
 def apart():
     theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
     try:
@@ -35,6 +60,59 @@ def apart():
     except:
         saveJson()
 
+# 保存坐标记录
+def saveJson():
+    with open(json_filename, 'w+') as f:
+        json.dump(record_list, f, ensure_ascii=False, indent=4)
+
+# 保存监控点
+def saveSite():
+    with open(site_info, 'w') as f:
+        json.dump(site_list, f, ensure_ascii=False, indent=4)
+
+# 重载监控点
+def load_site(site_info):
+    global site_list
+    try:
+        with open(site_info) as f:
+            site_list = json.load(f, encoding='utf8')
+    except:
+        saveSite()
+
+# 判断整数
+def is_instance(str):
+    try:
+        str = int(str)
+        return isinstance(str,int)
+    except ValueError:
+        return False
+
+# 添加监控点
+def add_site(server, args, info):
+    if len(args) == 7:
+        if args[2] in site_list:
+            server.tell(info.player, "§7[§aMonitor§f/§cWARN§7] §c监控点已存在")
+        elif is_instance(args[3])==0 or is_instance(args[4])==0 or is_instance(args[5])==0:
+            server.tell(info.player, "§7[§aMonitor§f/§cWARN§7] §c参数[x] [y] [z]必须为整数")
+        elif args[6]=='world' or args[6]=='nether' or args[6]=='end':
+            site_list[args[2]] = [args[3], args[4], args[5], args[6]]
+            server.say(f"§b{info.player} 添加了新的监控坐标 §e{args[2]}")
+        else:
+            server.tell(info.player, "§7[§aMonitor§f/§cWARN§7] §c参数[次元]错误")
+    else:
+        server.tell(info.player, "§7[§aMonitor§f/§cWARN§7] §c缺少参数，请输入!!mr 查看帮助信息")
+
+# 打印监控点
+def show_site(server):
+    server.say("§b[监控坐标点列表]")
+    for key, values in site_list.items():
+        x = int(values[0])
+        y = int(values[1])
+        z = int(values[2])
+        dim = values[3]
+        server.say(f"§a{key} §b次元: {dim}  §a{x}, {y}, {z}")
+
+# 控制坐标监控开关
 def on_player_joined(server, player):
     global count
     global status
@@ -42,7 +120,6 @@ def on_player_joined(server, player):
     if count == 1:
         status = 1
         monitor(server)
-
 def on_player_left(server, player):
     global count
     global status
@@ -50,7 +127,9 @@ def on_player_left(server, player):
     if count == 0:
         status = 0
 
+# 监控
 def monitor(server):
+    global record_list
     PI = server.get_plugin_instance('PlayerInfoAPI')
     OP = server.get_plugin_instance('OnlinePlayerAPI')
     while True:
@@ -64,15 +143,19 @@ def monitor(server):
                     y = int(pos[1])
                     z = int(pos[2])
                     dim = PI.getPlayerInfo(server, players[i], path='Dimension')
-                    pre = " 危 "
-                    if fp_x-50<=x<=fp_x+50 and fp_z-50<z<fp_z+50 and fp_y-10<=y<=fp_y+10 and dim==-1:
-                        server.say("§c"+ "Warning!" + pre + players[i] + pre + "在伪和平传送门附近游荡！！！")
-                    if dim == -1:
-                        dim = "Nether"
-                    elif dim == 0:
-                        dim = "World"
-                    elif dim == 1:
-                        dim = "End"
+                    if dim==0 or dim=="minecraft:overworld":
+                        dim = "world"
+                    elif dim==-1 or dim=="minecraft:the_nether":
+                        dim = "nether"
+                    elif dim==1 or dim=="minecraft:the_end":
+                        dim = "end"
+                    for key, values in site_list.items():
+                        fp_x = int(values[0])
+                        fp_y = int(values[1])
+                        fp_z = int(values[2])
+                        fp_dim = values[3]
+                        if fp_x-50<=x<=fp_x+50 and fp_z-50<z<fp_z+50 and fp_y-10<=y<=fp_y+10 and dim==fp_dim:
+                            server.say("§7[§aMonitor§f/§cWARNING§7]§c" + pre + players[i] + pre + "在 " + key + " 附近游荡！！！")
                     theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
                     info = str(theTime) + " " + str(players[i]) + " " + str(dim) + " x:" + str(x)+ " y:" + str(y)+ " z:" + str(z)
                     record_list.append(info)
@@ -82,3 +165,7 @@ def monitor(server):
             time.sleep(sleep-3)
         elif status == 0:
             break
+
+def on_unload(server):
+    saveJson()
+    saveSite()
--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# Monitor
-监控并记录玩家坐标
+# Monitor 2.0
+监控并记录玩家坐标，保存在records文件夹内
 
-在玩家靠近伪和平时向全服通知
+~~在玩家靠近伪和平时向全服通知~~
+
+玩家可自主添加需要监控的坐标点
 
 插件依赖[PlayerInfoAPI](https://github.com/TISUnion/PlayerInfoAPI)和[OnlinePlayerAPI](https://github.com/zhang-anzhi/MCDReforgedPlugins/tree/master/OnlinePlayerAPI)
 
-使用前先对插件进行设置
+使用前可先对插件进行设置：sleep = 12 ---- 每12秒记录一次玩家坐标
 
-sleep = 12 每12秒记录一次玩家坐标
+!!mr help  显示帮助消息
 
-fp_x = 0
+!!mr add [坐标名] [x] [y] [z] [次元]  次元：world nether end
 
-fp_y = 70
+!!mr list  显示所有已有监控
 
-fp_z = 0
+!!mr reload  重载监控坐标，联系管理员删除后操作该指令
 
-伪和平的地狱坐标为 0 70 0
+为防止随意删除更改监控点，删除监控请在后台操作，玩家仅有添加权限
